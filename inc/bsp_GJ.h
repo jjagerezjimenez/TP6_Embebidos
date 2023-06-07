570f05ea2bb2b4213b90a945b5c1081e2dcbd8ee
Juan Jose Armando Gerez Jimenez <jjagerezjimenez@gmail.com>
2023-06-07 00:16:32 -0300

    agrego poncho y modificaciones en bsp

--- inc/bsp_GJ.h
+++ inc/bsp_GJ.h
@@ -7,81 +7,19 @@
 #include <stdint.h>
 
 
-#define LED_R_PORT 2
-#define LED_R_PIN 0
-#define LED_R_FUNC SCU_MODE_FUNC4
-#define LED_R_GPIO 5
-#define LED_R_BIT 0
-
-#define LED_G_PORT 2
-#define LED_G_PIN 1
-#define LED_G_FUNC SCU_MODE_FUNC4
-#define LED_G_GPIO 5
-#define LED_G_BIT 1
-
-#define LED_B_PORT 2
-#define LED_B_PIN 2
-#define LED_B_FUNC SCU_MODE_FUNC4
-#define LED_B_GPIO 5
-#define LED_B_BIT 2
-
-#define LED_1_PORT 2
-#define LED_1_PIN 10
-#define LED_1_FUNC SCU_MODE_FUNC0
-#define LED_1_GPIO 0
-#define LED_1_BIT 14
-
-#define LED_2_PORT 2
-#define LED_2_PIN 11
-#define LED_2_FUNC SCU_MODE_FUNC0
-#define LED_2_GPIO 1
-#define LED_2_BIT 11
-
-#define LED_3_PORT 2
-#define LED_3_PIN 12
-#define LED_3_FUNC SCU_MODE_FUNC0
-#define LED_3_GPIO 1
-#define LED_3_BIT 12
-
-#define TEC_1_PORT 1
-#define TEC_1_PIN 0
-#define TEC_1_FUNC SCU_MODE_FUNC0
-#define TEC_1_GPIO 0
-#define TEC_1_BIT 4
-
-#define TEC_2_PORT 1
-#define TEC_2_PIN 1
-#define TEC_2_FUNC SCU_MODE_FUNC0
-#define TEC_2_GPIO 0
-#define TEC_2_BIT 8
-
-#define TEC_3_PORT 1
-#define TEC_3_PIN 2
-#define TEC_3_FUNC SCU_MODE_FUNC0
-#define TEC_3_GPIO 0
-#define TEC_3_BIT 9
-
-#define TEC_4_PORT 1
-#define TEC_4_PIN 6
-#define TEC_4_FUNC SCU_MODE_FUNC0
-#define TEC_4_GPIO 1
-#define TEC_4_BIT 9
-
-
 
 
 
 typedef struct board_s {
 
-    DigitalOutput_t Led_azul;
-    DigitalOutput_t Led_rojo;
-    DigitalOutput_t Led_amarillo;
-    DigitalOutput_t Led_verde;
+    DigitalOutput_t Buzzer;
 
-    DigitalInput_t  boton_prueba;
-    DigitalInput_t  boton_cambiar;
-    DigitalInput_t  boton_prender;
-    DigitalInput_t  boton_apagar;
+    DigitalInput_t  Set_time;
+    DigitalInput_t  Set_alarm;
+    DigitalInput_t  decrementar;
+    DigitalInput_t  incrementar;
+    DigitalInput_t  Aceptar;
+    DigitalInput_t  Cancelar;
 
 } const * const board_t;
 
